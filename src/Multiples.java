66f838ea239c6fbad9e6a080acdd55318ecdd30c
AbhigyanD <abhigyandey2@gmail.com>
2024-09-11 17:43:58 -0400

    Multiples

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,15 +1,22 @@
-public class Multiples
-{
-    public static void main(String[] args)
-    {
+public class Multiples {
+    public Multiples() {
+    }
+
+    public static int countMultiples(int n, int a, int b) {
         int count = 0;
-        for (int i = 1; i < 1000; i++)
-        {
-            if (i % 3 == 0 || i % 5 == 0)
-            {
-                count++;
+
+        // Start loop from 0 to include non-negative multiples
+        for(int i = 0; i < n; ++i) {
+            if (i % a == 0 || i % b == 0) {
+                ++count;
             }
         }
-        System.out.println("Multiples of 3 or 5 below 1000: " + count);
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int result = countMultiples(1000, 3, 5);
+        System.out.println("Multiples of 3 or 5 below 1000: " + result);
     }
 }
